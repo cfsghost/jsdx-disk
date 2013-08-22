764c689f8396058d5cc66c3d905dfa1d2eaf729d
Fred Chien <fred@mandice.com>
2013-08-22 23:36:37 +0800

    add createPartition API

---
+++ tests/create_partition.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Disk = require('../');
+
+var disk = new Disk();
+
+disk.init(function() {
+
+	var obj = disk.getStorageObject('Block', 'sdb');
+
+	console.log('Getting size of disk');
+	obj.getProperties('Block', function(err, props) {
+		console.log(props);
+		var availSize = props.Size - 1048576;
+
+		console.log('Create Partition ...');
+		obj.createPartition(1048576, availSize, '0x83', null, {}, function() {
+			console.log('Done');
+			process.exit();
+		});
+	});
+
+});
