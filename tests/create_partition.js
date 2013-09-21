ca135a1a18f14c0440c0d941c7391e3e9f15e5d3
Fred Chien <fred@mandice.com>
2013-09-22 04:11:54 +0800

    migrate to new node-dbus

--- tests/create_partition.js
+++ tests/create_partition.js
@@ -11,12 +11,14 @@ disk.init(function() {
 	console.log('Getting size of disk');
 	obj.getProperties('Block', function(err, props) {
 		var availSize = props.Size - 1048576;
+		console.log('Available size: ' + availSize);
 
 		console.log('Create Partition ...');
 		obj.createPartition(1048576, availSize, '0x83', null, {}, function() {
 			console.log('Done');
 			process.exit();
 		});
+
 	});
 
 });
